a3bc8b2525b901e127f9f2c8393d2c08f2cbed3a
JessinRa <jessin.bdg@gmail.com>
2018-02-09 00:18:03 +0700

    spiral iterate, skill check, map create, skill review

--- Attack_module.py
+++ Attack_module.py
@@ -1,146 +1,175 @@
-import time
+from Database import *
+
+""" *************************************************
+                DEBUG FUNCTION 
+************************************************* """
 
 
-search_list_5x5 = [(1, 0),
-                   (1, 1),
-                   (0, 1),
-                   (-1, 1),
-                   (-1, 0),
-                   (-1, -1),
-                   (0, -1),
-                   (1, -1),
-                   (2, -1),
-                   (2, 0),
-                   (2, 1),
-                   (2, 2),
-                   (1, 2),
-                   (0, 2),
-                   (-1, 2),
-                   (-2, 2),
-                   (-2, 1),
-                   (-2, 0),
-                   (-2, -1),
-                   (-2, -2),
-                   (-1, -2),
-                   (0, -2),
-                   (1, -2),
-                   (2, -2),
-                   ]
+def print_battle_map(battle_map):
+    """
+    Print battle map to console
+    """
 
-search_list_3x3 = [(0, 0),
-                   (1, 0),
-                   (1, 1),
-                   (0, 1),
-                   (-1, 1),
-                   (-1, 0),
-                   (-1, -1),
-                   (0, -1),
-                   (1, -1)]
+    for row in battle_map:
+        print(row)
 
-skill_aoe_double_h = [(1, 0), (-1, 0)]
-skill_aoe_double_v = [(0, 1), (0, -1)]
-skill_aoe_corner = [(-1, -1), (-1, 1), (1, 1), (1, -1)]
-skill_aoe_cross_d = [(0, 0), (-1, -1), (-1, 1), (1, 1), (1, -1)]
-skill_aoe_cross_h = [(0, 0), (-1, 0), (0, 1), (1, 0), (0, -1)]
 
-skill_aoe_seeker = []
 
-tile_mode_missed = "!"
-tile_mode_neutral = "~"
-tile_mode_shield = "@"
-tile_mode_hit = "*"
+""" *************************************************
+                USABLE FUNCTION 
+************************************************* """
 
 
 def create_battle_map(row, col):
+    """
+    Create matrix rox x col, initialize with neutral tile
+    :param row: battle map row
+    :type row: int
+    :param col: battle map col
+    :type col: int
+    :return: battle map
+    :rtype: list of list
+    """
+
     battle_map = []
     for x in range(0, row):
 
         new_row = []
         for y in range(0, col):
-            new_row.append("~")
+            new_row.append(tile_mode_neutral)
 
         battle_map.append(new_row)
 
     return battle_map
 
 
-def print_battle_map(battle_map):
 
-    for row in battle_map:
-        print(row)
 
 
 def attack_mode_examine_map(battle_map, pivot_x, pivot_y, usable_skill):
+    """
+    Function to examine 5x5 tiles around pivot, to find best skill to use on best position
+    :param battle_map: battle_map
+    :type battle_map: list of list
+    :param pivot_x: searching pivot x
+    :type pivot_x: int
+    :param pivot_y: searching pivot y
+    :type pivot_y: int
+    :param usable_skill: list of usable skill to check
+    :type usable_skill: list
+    :return:
+    :rtype:
+    """
 
-
-
+    # Iterate spiral (inside - out) 5x5 from pivot
     for dx, dy in search_list_5x5:
         try:
 
             current_x = pivot_x + dx
             current_y = pivot_y + dy
 
+            # For each skill, evaluate it
             for skill in usable_skill:
                 examine_result = examine_skill_effect(battle_map, current_x, current_y, skill)
-
 
         except:
             pass
 
 
 def examine_skill_effect(battle_map, pivot_x, pivot_y, skill_type):
+    """
+    Function to examine skill effect if activate on pivot
+    :param battle_map: battle_map
+    :type battle_map: list of list
+    :param pivot_x: pivot of skill activation (X)
+    :type pivot_x: int
+    :param pivot_y: pivot of skill activation (Y)
+    :type pivot_y: int
+    :param skill_type: type of skill used
+    :type skill_type: string
+    :return: examine result
+    :rtype: dict
+    """
 
+    def tile_examine_template():
+        """
+        Create basic template
+        :return: template
+        :rtype: dict
+        """
+
+        return {"neutral": 0,
+                "missed": 0,
+                "hit": 0,
+                "shield": 0}
+
+    def get_search_list():
+        """
+        Function to get search list based on skill typ
+        :return: skill AOE
+        :rtype: list
+        """
+
+        if skill_type == "double_h":
+            search_list = skill_aoe_double_h
+        elif skill_type == "double_v":
+            search_list = skill_aoe_double_v
+        elif skill_type == "corner":
+            search_list = skill_aoe_corner
+        elif skill_type == "cross_d":
+            search_list = skill_aoe_cross_d
+        elif skill_type == "cross_h":
+            search_list = skill_aoe_cross_h
+        # elif skill_type == "seeker":
+        #     search_list = skill_aoe_seeker
+        else:
+            search_list = []
+
+        return search_list
+
+    def tile_examine(tile):
+        """
+        Function to Examine each tile
+        :param tile:
+        :type tile:
+        :return:
+        :rtype:
+        """
+
+        if tile == tile_mode_shield:
+            return "shield"
+        elif tile == tile_mode_missed:
+            return "missed"
+        elif tile == tile_mode_hit:
+            return "hit"
+        else:
+            return "neutral"
+
+    # Create basic template
     examine_result = tile_examine_template()
 
     # Get list of tile to search
-    if skill_type == "double_h":
-        search_list = skill_aoe_double_h
-    elif skill_type == "double_v":
-        search_list = skill_aoe_double_v
-    elif skill_type == "corner":
-        search_list = skill_aoe_corner
-    elif skill_type == "cross_d":
-        search_list = skill_aoe_cross_d
-    elif skill_type == "cross_h":
-        search_list = skill_aoe_cross_h
-    # elif skill_type == "seeker":
-    #     search_list = skill_aoe_seeker
-    else:
-        search_list = []
+    search_list = get_search_list()
 
     # Check tile one by one
     for dx, dy in search_list:
         try:
 
-            # Do something
+            # Examine
             tile_status = tile_examine(battle_map[pivot_x + dx][pivot_y + dy])
             examine_result[tile_status] += 1
 
         except:
             pass
 
+    # Print result for visual check only
     print(examine_result)
     return examine_result
 
 
-def tile_examine(tile):
-
-    if tile == tile_mode_shield:
-        return "shield"
-    elif tile == tile_mode_missed:
-        return "missed"
-    elif tile == tile_mode_hit:
-        return "hit"
-    else:
-        return "neutral"
 
 
-def tile_examine_template():
 
-    return {"neutral": 0,
-            "missed": 0,
-            "hit": 0,
-            "shield": 0}
 
 
 def test_aoe(aoe):
@@ -165,10 +194,9 @@ def test_all_aoe():
         print(each)
         test_aoe(each)
 
-if __name__ == '__main__':
 
-    battle_map = create_battle_map(10, 10)
-    battle_map[4][4] = "X"
+
+def test_populate_map(battle_map):
     battle_map[5][4] = "!"
     battle_map[3][1] = "!"
     battle_map[4][2] = "*"
@@ -176,6 +204,21 @@ if __name__ == '__main__':
     battle_map[3][3] = "*"
     battle_map[2][2] = "*"
     battle_map[3][5] = "!"
+    battle_map[7][6] = "!"
+    battle_map[5][3] = "!"
+    battle_map[6][4] = "*"
+    battle_map[6][3] = "@"
+    battle_map[5][5] = "*"
+    battle_map[8][5] = "*"
+    battle_map[6][7] = "!"
+
+
+
+if __name__ == '__main__':
+
+    battle_map = create_battle_map(10, 10)
+    battle_map[4][4] = "X"
+    test_populate_map(battle_map)
     print_battle_map(battle_map)
 
 
