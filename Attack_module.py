b64ff5d4f26d84ea5bc7e04f9cebeb26159f2752
JessinRa <jessin.bdg@gmail.com>
2018-02-10 21:22:22 +0700

    fine tuning

--- Attack_module.py
+++ Attack_module.py
@@ -1,4 +1,4 @@
-from Database import *
+from God_eye import *
 
 """ *************************************************
                 DEBUG FUNCTION 
@@ -56,7 +56,7 @@ def test_populate_map(battle_map):
     battle_map[3][1] = "!"
     battle_map[4][2] = "*"
     battle_map[4][1] = "@"
-    battle_map[3][3] = "*"
+    battle_map[3][4] = "*"
     battle_map[2][2] = "*"
     battle_map[3][5] = "!"
     battle_map[7][6] = "!"
@@ -65,6 +65,21 @@ def test_populate_map(battle_map):
     battle_map[6][3] = "@"
     battle_map[5][5] = "*"
     battle_map[8][5] = "*"
+    battle_map[6][7] = "!"
+
+    battle_map[3][2] = "!"
+    battle_map[5][1] = "!"
+    battle_map[8][3] = "*"
+    battle_map[4][7] = "@"
+    battle_map[3][8] = "*"
+    battle_map[3][3] = "*"
+    battle_map[6][6] = "!"
+    battle_map[3][6] = "!"
+    battle_map[7][7] = "!"
+    battle_map[7][2] = "*"
+    battle_map[2][6] = "@"
+    battle_map[7][4] = "*"
+    battle_map[4][2] = "*"
     battle_map[6][7] = "!"
 
 
@@ -96,20 +111,21 @@ def create_battle_map(row, col):
     return battle_map
 
 
-def attack_mode_examine_map(battle_map, pivot_x, pivot_y, usable_skill):
+def attack_mode_examine_map(battle_map, pivot, usable_skill):
     """
     Function to examine 5x5 tiles around pivot, to find best skill to use on best position
     :param battle_map: battle_map
     :type battle_map: list of list
-    :param pivot_x: searching pivot x
-    :type pivot_x: int
-    :param pivot_y: searching pivot y
-    :type pivot_y: int
+    :param pivot: searching pivot
+    :type pivot: int,int
     :param usable_skill: list of usable skill to check
     :type usable_skill: list
-    :return:
+    :return: examine report
     :rtype:
     """
+
+    pivot_x, pivot_y = pivot
+    examine_report = []
 
     # Iterate spiral (inside - out) 5x5 from pivot
     for dx, dy in search_list_5x5:
@@ -120,21 +136,26 @@ def attack_mode_examine_map(battle_map, pivot_x, pivot_y, usable_skill):
 
             # For each skill, evaluate it
             for skill in usable_skill:
-                examine_result = examine_skill_effect(battle_map, current_x, current_y, skill)
+                examine_result = examine_skill_effect(battle_map, (current_x, current_y), skill)
 
+                examine_result["skill"] = skill
+                examine_result["x"] = current_x
+                examine_result["y"] = current_y
+
+                examine_report.append(examine_result)
         except:
             pass
 
+    return examine_report
 
-def examine_skill_effect(battle_map, pivot_x, pivot_y, skill_type):
+
+def examine_skill_effect(battle_map, pivot, skill_type):
     """
     Function to examine skill effect if activate on pivot
     :param battle_map: battle_map
     :type battle_map: list of list
-    :param pivot_x: pivot of skill activation (X)
-    :type pivot_x: int
-    :param pivot_y: pivot of skill activation (Y)
-    :type pivot_y: int
+    :param pivot: pivot of skill activation
+    :type pivot: int,int
     :param skill_type: type of skill used
     :type skill_type: string
     :return: examine result
@@ -148,10 +169,14 @@ def examine_skill_effect(battle_map, pivot_x, pivot_y, skill_type):
         :rtype: dict
         """
 
-        return {"neutral": 0,
+        return {"skill": None,
+                "x": None,
+                "y": None,
+                "neutral": 0,
                 "missed": 0,
                 "hit": 0,
-                "shield": 0}
+                "shield": 0,
+                "potential": 0}
 
     def get_search_list():
         """
@@ -192,8 +217,15 @@ def examine_skill_effect(battle_map, pivot_x, pivot_y, skill_type):
             return "missed"
         elif tile == tile_mode_hit:
             return "hit"
+        elif tile == tile_mode_hit:
+            return "potential"
         else:
             return "neutral"
+
+    # def mark_potential(tile):
+
+    # Separate x and y
+    pivot_x, pivot_y = pivot
 
     # Create basic template
     examine_result = tile_examine_template()
@@ -212,21 +244,71 @@ def examine_skill_effect(battle_map, pivot_x, pivot_y, skill_type):
         except:
             pass
 
-    # Print result for visual check only
-    print(examine_result)
     return examine_result
 
 
+def greedy_pick(player, examine_report):
+    """
+    Find best record
+    :param player:
+    :type player:
+    :param examine_report:
+    :type examine_report:
+    :return:
+    :rtype:
+    """
 
+    best_record = examine_report[0]
+    skill_cost = player.cost
+    max_value = 0
+
+    # Coefficient to adjust
+    p_coef = 125
+    n_coef = 20
+
+    m_coef = -20
+    h_coef = -20
+    s_coef = -20
+
+    # Calculate
+    for examine_result in examine_report:
+
+        value = 0
+        value += examine_result["potential"] * p_coef
+        value += examine_result["neutral"] * n_coef
+
+        value += examine_result["missed"] * m_coef
+        value += examine_result["hit"] * h_coef
+        value += examine_result["shield"] * s_coef
+
+        not_hit = examine_result["potential"] + examine_result["neutral"]
+        already_hit = examine_result["missed"] + examine_result["hit"] + examine_result["shield"] + 1
+
+        t_coef = not_hit / (already_hit + not_hit)
+        value *= t_coef
+
+        c_coef = (already_hit + not_hit) / skill_cost[examine_result["skill"]]
+        value *= c_coef
+
+        if value > max_value:
+            max_value = value
+            best_record = examine_result
+
+    return best_record
 
 
 if __name__ == '__main__':
 
+    # TESTING PURPOSE
     battle_map = create_battle_map(10, 10)
     battle_map[4][4] = "X"
     test_populate_map(battle_map)
     print_battle_map(battle_map)
 
+    # BOT SECTION
+    megumi = Player(filename)
+    last_hit = (4,4)
+    examine_report = attack_mode_examine_map(battle_map, pivot=last_hit, usable_skill=megumi.usable_skill)
 
-    usable_skill = ["cross_d", "double_h", "double_v", "corner"]
-    attack_mode_examine_map(battle_map, pivot_x=4, pivot_y=4, usable_skill=usable_skill)
+    best_result = greedy_pick(megumi, examine_report)
+    print(best_result)
